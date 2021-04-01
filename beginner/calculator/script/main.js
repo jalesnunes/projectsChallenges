09d1ee6113170e851fa06b0fe6ccdb0ff806c4ff
Jales Nunes <jales.nunes@hotmail.com>
2021-03-31 18:00:20 -0600

    Calculator project done

--- beginner/calculator/script/main.js
+++ beginner/calculator/script/main.js
@@ -1,3 +1,40 @@
-function addNumber(num) {
-    document.getElementById("visor2").innerHTML += num
+//const numCollection = []
+
+function insertData(num) {
+    document.getElementById("visor").innerHTML += num
+}
+
+/*function calculation(symbol) {
+    var oldNum = document.getElementById("visor").innerHTML
+    numCollection.push(Number(oldNum))
+
+    document.getElementById("accumulator").innerHTML += ` ${document.getElementById("visor").innerHTML} ${symbol}`
+
+    console.log(numCollection)
+
+    document.getElementById("visor").innerHTML = ""
+    numCollection.push(symbol)
+}*/
+
+function showResult() {
+    document.getElementById("accumulator").innerHTML = `${document.getElementById("visor").innerHTML} ${"="} `
+    var result = eval(document.getElementById("visor").innerHTML)
+
+    document.getElementById("visor").innerHTML = result
+
+    document.getElementById("accumulator").innerHTML += result
+}
+
+function cleanAll() {
+    document.getElementById("visor").innerHTML = ""
+    document.getElementById("accumulator").innerHTML = ""
+    /*while (numCollection.length) {
+        numCollection.pop()
+    }
+
+    console.log(numCollection)*/
+}
+
+function cleanThis() {
+    document.getElementById("visor").innerHTML = ""
 }
