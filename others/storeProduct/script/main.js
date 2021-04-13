40e95d215ad5a97ab8791c551b84c4a23d4ad783
Jales Nunes <jales.nunes@hotmail.com>
2021-04-12 19:28:46 -0600

    Product item project completed

---
+++ others/storeProduct/script/main.js
@@ -0,0 +1,11 @@
+function changeToRed() {
+    document.getElementById("shoes").src = "/images/shoesRed.png"
+}
+
+function changeToGreen() {
+    document.getElementById("shoes").src = "/images/shoesGreen.png"
+}
+
+function changeToBlue() {
+    document.getElementById("shoes").src = "/images/shoesBlue.png"
+}
